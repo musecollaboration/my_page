049994db14ee33b55c583d5c71e04dc6570345e9
Alexander Terekhov <cahdro2155@gmail.com>
2025-01-26 19:29:28 +0300

    Написал тесты для приложения horoscope

--- horoscope/tests.py
+++ horoscope/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from . import views
 
-# Create your tests here.
+
+class TestHoroscope(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/horoscope/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_libra(self):
+        response = self.client.get('/horoscope/libra/')
+        self.assertEqual(response.status_code, 200)
+        pattern = 'Весы - седьмой знак зодиака. Рожден 24 сентября - 23 октября'
+        self.assertIn(pattern, response.content.decode())
+
+    def test_libra_redirect(self):
+        for i in range(1, 13):
+            redirect_url = reverse('horoscope-num', args=[i])
+            response = self.client.get(redirect_url)
+            self.assertEqual(response.status_code, 302)
+            pattern = f'/horoscope/{list(views.dict_zodiac)[i-1]}/'
+            self.assertEqual(response.url, pattern)
+
+    def test_signs(self):
+        for sign in views.dict_zodiac:
+            redirect_url = reverse('horoscope-name', args=[sign])
+            response = self.client.get(redirect_url)
+            self.assertEqual(response.status_code, 200)
+            pattern = views.dict_zodiac[sign]
+            self.assertIn(pattern, response.content.decode())
--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 
@@ -6,7 +7,7 @@ urlpatterns = [
     path('', views.index),
     path('type/', views.get_info_type),
     path('type/<element>/', views.get_info_type_elemens, name='get_info_type_elemens'),
-    path('<int:sign_zodiac>/', views.get_info_about_sing_zodiac_num),
+    path('<int:sign_zodiac>/', views.get_info_about_sing_zodiac_num, name='horoscope-num'),
     path('<str:sign_zodiac>/', views.get_info_about_sing_zodiac, name='horoscope-name'),
     path('<int:month>/<int:day>', views.get_zodiac_date),
 ]
